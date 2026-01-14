2fd585734001a9fdd40402ad955a8611ac504e95
Oleg Kuzmin <okuzmin@teh-lab.ru>
2026-01-14 22:55:32 +0300

    fix(task2): fix proxy service

--- src/microservices/proxy/app/main.py
+++ src/microservices/proxy/app/main.py
@@ -1,9 +1,15 @@
-from fastapi import Response, FastAPI
+from fastapi import Response, Request, FastAPI
 import json
 import os
 import requests
 import random
 from fastapi.responses import JSONResponse
+
+EXCLUDED_HEADERS = {
+    "content-length",
+    "transfer-encoding",
+    "connection",
+}
 
 port = os.getenv("PORT", "80")
 MONOLITH_URL = os.getenv("MONOLITH_URL")
@@ -16,26 +22,42 @@ app = FastAPI()
 @app.get("/api/movies")
 @app.post("/api/movies")
 async def movies_proxy(request: Request, q: str | None = None):
+    body = await request.body()
     if GRADUAL_MIGRATION == 'true' and random.randrange(1, 100) < MOVIES_MIGRATION_PERCENT:
-        r = requests.request(request.method, f'{MOVIES_SERVICE_URL}{request.url.path}{q}', data=await request.json())
+        r = requests.request(request.method,
+                             f'{MOVIES_SERVICE_URL}{request.url.path}',
+                             data=body,
+                             params=request.query_params,)
     else:
-        r = requests.request(request.method, f'{MONOLITH_URL}{request.url.path}{q}', data=await request.json())
+        r = requests.request(request.method,
+                             f'{MONOLITH_URL}{request.url.path}',
+                             data=body,
+                             params=request.query_params,)
 
     return Response(
         content=r.content,
         status_code=r.status_code,
-        headers=dict(r.headers) # Convert requests headers (CaseInsensitiveDict) to a standard dict
+        headers={
+            k: v for k, v in r.headers.items() if k.lower() not in EXCLUDED_HEADERS
+        }
     )
 
 
 @app.get("/api/users")
 @app.post("/api/users")
 async def users_proxy(request: Request, q: str | None = None):
-    r = requests.request(request.method, f'{MONOLITH_URL}{request.url.path}{q}', data=await request.json())
+    body = await request.body()
+    r = requests.request(request.method,
+                         f'{MONOLITH_URL}{request.url.path}',
+                         data=body,
+                         params=request.query_params,)
+
     return Response(
         content=r.content,
         status_code=r.status_code,
-        headers=dict(r.headers) # Convert requests headers (CaseInsensitiveDict) to a standard dict
+        headers={
+            k: v for k, v in r.headers.items() if k.lower() not in EXCLUDED_HEADERS
+        }
     )
 
 @app.get("/health")
